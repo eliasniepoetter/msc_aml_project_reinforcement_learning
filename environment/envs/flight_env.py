47d8b935fd21f6b5cb2544f22cdc91bb1fe21d09
Johannes Kaufmann <st161947@stud.uni-stuttgart.de>
2024-01-06 10:07:07 +0100

    structure environment

--- environment/envs/flight_env.py
+++ environment/envs/flight_env.py
@@ -1,22 +1,65 @@
-import gymnasium as gym
-from gymnasium import spaces
+from gymnasium import Env
+from gymnasium.spaces import Dict, Box
+import numpy as np
+from dynamics import Dynamics
 
-class FlightEnv(gym.Env):
+class FlightEnv(Env):
+
     def __init__(self):
-        # Todo: define action and observation space
-        self.action_space = spaces.Box(low=0, high=1, shape=(1, 1), dtype=np.float32)
-        self.observation_space = spaces.Box(low=0, high=1, shape=(1, 1), dtype=np.float32)
-
-    def step(self, action):
-        pass
-        return observation, reward, terminated, done, info
+        # Todo: define boundaries for action and observation space
+        # defintion of spaces
+        self.observation_space = Dict({'velocity':Box(low=0, high=1, shape=(1, 1), dtype=np.float32),
+                                       'alpha':Box(low=-1, high=1, shape=(1, 1), dtype=np.float32),
+                                       'pos_z':Box(low=0, high=1, shape=(1, 1), dtype=np.float32),
+                                       'pos_x':Box(low=0, high=1, shape=(1, 1), dtype=np.float32),
+                                       'q':Box(low=-1, high=1, shape=(1, 1), dtype=np.float32),
+                                       'Theta':Box(low=-1, high=1, shape=(1, 1), dtype=np.float32)})
+        self.action_space = Dict({'elevator':Box(low=-1, high=1, shape=(1, 1), dtype=np.float32),
+                                  'throttle':Box(low=0, high=1, shape=(1, 1), dtype=np.float32)})
+        
+        # initial condition and state
+        self.reset()
 
     def reset(self):
-        pass
+        # ToDo: set dynamics to initial state
+        self.current_step = 0
+        self.reward = 0
+        initial_state = None # ToDo: define initial state
+        
+        # check if reset is called for the first time
+        if not self.dynamics in locals():
+            self.dynamics = Dynamics()
+
+        # set state of dynamics to initial state and convert to observation
+        self.dynamics.state = initial_state
+        observation = self._state_to_observation(initial_state)
+
         return observation
+    
+    def step(self, action):
+        # ToDo: convert action to input for dynamics and observation from output of dynamics
+        state = Dynamics.updateStates(action,printState=False)
+        observation = self._state_to_observation(state)
+        
+        # ToDo: implement reward function
+        reward = self.reward
+        self.current_step += 1
+        
+        # ToDo: implement termination condition
+        terminated = False
+        
+        # ToDo: implement done condition
+        done = False
+        
+        return observation, reward, terminated, done, info
 
     def render(self):
+        # ToDo: implement flight visualization with flightgear
         pass
 
     def close(self):
+        pass
+
+    def _state_to_observation(self, state):
+        # ToDo: implement conversion from state to observation based on observation space and state definition
         pass
