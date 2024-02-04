71035c0027cad48aedb3871c523abfa04a09dca4
Lucas <l.schratt@yahoo.de>
2024-02-04 13:23:30 +0100

    changed train.py

--- training/train.py
+++ training/train.py
@@ -4,6 +4,9 @@ from stable_baselines3 import DDPG
 from stable_baselines3 import DQN
 from stable_baselines3 import SAC
 from stable_baselines3 import TD3
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.evaluation import evaluate_policy
+import gymnasium as gym
 from custom_callback.successCallback import SuccessCallback
 from stable_baselines3.common.callbacks import BaseCallback ,EvalCallback, StopTrainingOnRewardThreshold, CheckpointCallback
 from stable_baselines3.common.vec_env import VecNormalize
@@ -13,16 +16,39 @@ import time
 
 # Initialize callback
 # callback = SuccessCallback(check_freq=1) 
-callback = StopTrainingOnRewardThreshold(reward_threshold=900) #currently not used
 
 #initializing environment
 env = FlightEnvTargetAltitude()
 # env = VecNormalize(env)
+
+# wrapper for environment for callback and monitoring
+env = Monitor(env, filename='./results/monitor', allow_early_resets=True)
+
+# reset at the before start of training
 env.reset()
+
+callback_stop = StopTrainingOnRewardThreshold(reward_threshold=100) #currently not used
+callback = EvalCallback(env, callback_after_eval=callback_stop, 
+                        best_model_save_path='./results/eval/models/', log_path='./results/eval/logs/',
+                          eval_freq=5000, deterministic=True, render=True, n_eval_episodes=10, verbose=1)
 
 # to use tensorboard log open terminal and run: tensorboard --logdir ./results/ppo_tensorboard/<subdirectory generated>
 # TODO: add criteria to stop when 100 epoch has been successfull
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="./results/ppo_tensorboard/").learn(total_timesteps=100000000, progress_bar=True)
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="./results/ppo_tensorboard/").learn(total_timesteps=10000, progress_bar=True, callback=callback)
+
+
+rewards_list = env.get_episode_rewards()
+episode_length = env.get_episode_lengths()
+
+
+# evalute the policy
+
+# if other model than current model should be evaluated, load the model and set the environment
+# model = PPO.load("ppo_flight_env")
+# model.set_env(env)
+
+episode_reward_list, episode_length_list = evaluate_policy(model, env, n_eval_episodes=1, render=True, deterministic=True, return_episode_rewards=True, warn=True)
+
 
 #this is for testing purposes
 for i in range(5):
