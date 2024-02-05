f3006a7cae3a82c3e4d036fb8c4bfe050cb7384e
Johannes Kaufmann <st161947@stud.uni-stuttgart.de>
2024-02-05 16:16:44 +0100

    ready to train

--- environment/visualization.py
+++ environment/visualization.py
@@ -17,36 +17,42 @@ class PlotVisualizer():
         ax_alpha    = plt.subplot(3, 3, 1)
         ax_alpha.plot(t, episode_data[:,0])
         ax_alpha.set_ylabel('alpha [rad]')
+        ax_alpha.grid(True)
 
         # q
         ax_q        = plt.subplot(3, 3, 2)         
         ax_q.plot(t, episode_data[:,1])
         ax_q.set_ylabel('q [rad/s]')
-        
+        ax_q.grid(True)
+
         # V
         ax_V        = plt.subplot(3, 3, 4)
-        ax_V.plot(t, episode_data[:,2]+51.4, lable='V')
+        ax_V.plot(t, episode_data[:,2]+51.4, label='V')
         ax_V.plot(t, episode_data[:,4], label='v_x', linestyle='-.')
         ax_V.plot(t, episode_data[:,5], label='v_z', linestyle='--')
         ax_V.set_ylabel('V [m/s]')
         ax_V.legend()
+        ax_V.grid(True)
 
         # theta
         ax_theta    = plt.subplot(3, 3, 5)          
         ax_theta.plot(t, episode_data[:,3])
         ax_theta.set_ylabel('theta [rad]')
+        ax_theta.grid(True)
 
         # elevator
         ax_elevator = plt.subplot(3, 3, 7)          
         ax_elevator.plot(t, episode_data[:,-2])
         ax_elevator.set_ylabel('elevator')
         ax_elevator.set_xlabel('t [s]')
-        
+        ax_elevator.grid(True)
+
         # throttle
         ax_throttle = plt.subplot(3, 3, 8)          
         ax_throttle.plot(t, episode_data[:,-1])
         ax_throttle.set_ylabel('throttle')
         ax_throttle.set_xlabel('t [s]')
+        ax_throttle.grid(True)
 
         # x-z plot
         ax_xz       = plt.subplot(3, 3, (3,9))    
@@ -54,7 +60,9 @@ class PlotVisualizer():
         ax_xz.axhline(y=target_altitude, linestyle='--')
         ax_xz.set_xlabel('x [m]')
         ax_xz.set_ylabel('z [m]')
+        ax_xz.grid(True)
 
+        plt.tight_layout()
         plt.show()
     
     def close(self):
