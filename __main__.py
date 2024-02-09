74eb1400f5a0f38ea901abb4c7599eecbc69bd4a
Johannes Kaufmann <st161947@stud.uni-stuttgart.de>
2024-02-09 08:47:01 +0100

    minor changes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from training import train
 
 ppo_flight_env = train.PPOTraining()
-ppo_flight_env.train(total_timesteps=1000000)
-ppo_flight_env.evaluate(episode_count=5, model_name=None)
+#ppo_flight_env.train(total_timesteps=10000000)
+#! change model name to evaluate different model, or None to evaluate current model
+ppo_flight_env.evaluate(episode_count=5, model_name=r"results\eval\models\best_model.zip") 
